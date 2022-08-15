3b6d962fdec72cb3f2c1fbe4f92ca625099c9a21
Misgan Moges <misganmoges03@gmail.com>
2022-08-15 13:04:19 +0300

    second commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
 
-const url = "https://course-api.com/react-tours-project";
+const url = "https://course-api.netlify.app/api/react-tours-project";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
